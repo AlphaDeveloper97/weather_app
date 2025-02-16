a58930fa9a3857948c6642c167dd75a5c94c2baf
Mostafa Ragab <mostafaragabelmshad@gmail.com>
2025-02-16 04:31:50 +0200

    create custom text filed

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/widgets/custom_text.dart';
+import 'package:weather_app/widgets/custom_text_filed.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -8,7 +9,10 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: CustomText(text: "Search City", fontSize: 22)),
-      body: ,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(child: CustomTextFiled()),
+      ),
     );
   }
 }
--- lib/widgets/custom_text_filed.dart
+++ lib/widgets/custom_text_filed.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/custom_text.dart';
 
 class CustomTextFiled extends StatelessWidget {
   const CustomTextFiled({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const TextField();
+    return TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.only(top: 30, bottom: 30, left: 22),
+        label: CustomText(text: "Search", fontSize: 22),
+        labelStyle: TextStyle(fontSize: 16),
+        hintText: "Search",
+        hintStyle: TextStyle(fontSize: 16),
+        suffixIcon: Icon(Icons.search),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(color: Colors.black),
+        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+      ),
+    );
   }
 }
