35ca2e5f4c882b559aebeb5c3228228f5ffae67d
Mostafa Ragab <mostafaragabelmshad@gmail.com>
2025-02-16 08:20:59 +0200

    create weather service and get weather cubit  and finished them

--- lib/cubits/weather cubit/get_weather_cubit.dart
+++ lib/cubits/weather cubit/get_weather_cubit.dart
@@ -1,6 +1,20 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/weather%20cubit/get_weather_state.dart';
+import 'package:weather_app/services/weather_service.dart';
+import 'package:weather_app/views/models/weather_model.dart';
 
 class GetWeatherCubit extends Cubit {
   GetWeatherCubit() : super(WeatherInaitialState());
+
+  WeatherModel? weatherModel;
+
+  getWeather(String city) {
+    try {
+      weatherModel = WeatherService(Dio()).getweatherservice(city);
+      emit(WeatherLoadedState(weatherModel!));
+    } catch (e) {
+      emit(WeatherFailureState(e.toString()));
+    }
+  }
 }
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,32 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:weather_app/views/models/weather_model.dart';
+
+class WeatherService {
+  final Dio dio;
+
+  WeatherService(this.dio);
+
+  final String basUrl = "http://api.weatherapi.com/v1";
+  final String apikey = "5c8ad8994abc4aeb90062029250601";
+
+  getweatherservice(String city) async {
+    try {
+      Response response = await dio.get(
+        "http://api.weatherapi.com/v1/forecast.json?key=5c8ad8994abc4aeb90062029250601&q=$city&days=1",
+      );
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errMessage =
+          e.response?.data["error"]["message"] ??
+          "Opps there is an error, try later";
+      throw Exception(errMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception("Opps there is an error, try later");
+    }
+  }
+}
